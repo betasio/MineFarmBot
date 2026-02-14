bec5b7a9c94a595f9c049e87f46cb3ca3911c278
Alan Albino <132348292+betasio@users.noreply.github.com>
2026-02-14 22:58:36 +0000

    Lock launcher actions while bot launch is in progress

---
+++ desktop/preload.js
@@ -0,0 +1,18 @@
+'use strict'
+
+const { contextBridge, ipcRenderer } = require('electron')
+
+contextBridge.exposeInMainWorld('minefarmDesktop', {
+  isDesktop: true,
+  restartBot: () => ipcRenderer.invoke('desktop:restart-bot'),
+  listProfiles: () => ipcRenderer.invoke('desktop:list-profiles'),
+  createProfile: (payload) => ipcRenderer.invoke('desktop:create-profile', payload),
+  launchProfile: (profileId) => ipcRenderer.invoke('desktop:launch-profile', profileId),
+  deleteProfile: (profileId) => ipcRenderer.invoke('desktop:delete-profile', profileId),
+  stopBot: () => ipcRenderer.invoke('desktop:stop-bot'),
+  onAuthCode: (handler) => {
+    const listener = (_event, payload) => handler(payload)
+    ipcRenderer.on('desktop:msa-code', listener)
+    return () => ipcRenderer.removeListener('desktop:msa-code', listener)
+  }
+})
